68190e27f2c33ea69fe8670c0267096a81dbafab
ravi1718 <neeliravitej@gmail.com>
2025-04-14 16:56:57 +0530

    commit

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -1,4 +1,4 @@
-import { useUser, UserButton } from "@clerk/clerk-react";
+import { useUser, } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
